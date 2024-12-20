8db5934f80020d9e999de6196301be73cc1c46c2
Derek Johnston <101991363+usethisname1419@users.noreply.github.com>
2024-12-20 10:49:57 -0600

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,11 +1,11 @@
 import requests
 import json
 from datetime import datetime, timedelta
-
+from logger import print_info, print_error
 def fetch_cve_data():
     # Calculate date range for the last week
     end_date = datetime.utcnow()
-    start_date = end_date - timedelta(days=7)
+    start_date = end_date - timedelta(days=25)
 
     # Format dates in ISO 8601
     start_date_str = start_date.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
@@ -21,15 +21,13 @@ def fetch_cve_data():
     }
 
     # Fetch data
-    print(f"[INFO] Fetching CVEs published between {start_date_str} and {end_date_str}.")
+    print_info("Fetching CVEs published between {start_date_str} and {end_date_str}.")
     response = requests.get(url, params=params)
     if response.status_code == 200:
         cve_data = response.json()
         with open("cve_data.json", "w") as file:
             json.dump(cve_data, file, indent=4)
-        print("[INFO] CVE data fetched and saved to 'cve_data.json'.")
+        print_info("CVE data fetched and saved to 'cve_data.json'.")
     else:
-        print(f"[ERROR] Failed to fetch CVE data. HTTP Status Code: {response.status_code}")
+        print_error(f"Failed to fetch CVE data. HTTP Status Code: {response.status_code}")
 
-if __name__ == "__main__":
-    fetch_cve_data()
